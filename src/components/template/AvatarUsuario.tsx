5347218890f7eb004ca3d05ba34c4ec06322a4a6
sirAndre1337 <andrelluis17@hotmail.com>
2022-09-02 18:27:00 -0300

    Criando avatar do usuario

---
+++ src/components/template/AvatarUsuario.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import useAuth from "../../data/hook/useAuth";
+
+
+interface AvatarUsuarioProps {
+    className?: string;
+}
+
+export default function AvatarUsuario(props: AvatarUsuarioProps) {
+
+    const {usuario} = useAuth();
+
+    return (
+        <Link href="/perfil">
+            <img 
+            src={usuario?.imagemUrl ?? '/images/avatar.svg'}
+             alt="Imagem perfil usuario" 
+             className={`h-10 w-10 rounded-full cursor-pointer ${props.className}`}
+             />
+        </Link>
+    )
+}
---
+++ src/pages/perfil.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/template/Layout";
+
+export default function Perfil() {
+  return (
+    <div>
+      <Layout titulo="Perfil" subtitulo="Personalize seu perfil">
+        filhos
+      </Layout>
+    </div>
+  )
+}

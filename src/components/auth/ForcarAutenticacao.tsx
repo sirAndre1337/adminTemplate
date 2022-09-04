fc703d1cb5892a4a735107fa56735ee623024f96
sirAndre1337 <andrelluis17@hotmail.com>
2022-09-04 17:32:12 -0300

    function para verificar se tem cookie

--- src/components/auth/ForcarAutenticacao.tsx
+++ src/components/auth/ForcarAutenticacao.tsx
@@ -1,15 +1,27 @@
+import Head from "next/head";
 import Image from "next/image"
 import Router from "next/router";
 import loading from '../../../public/images/loading.gif';
 import useAuth from "../../data/hook/useAuth";
 
-export default function ForcarAutenticacao(props){
+export default function ForcarAutenticacao(props) {
 
-    const {usuario , carregando} = useAuth();
+    const { usuario, carregando } = useAuth();
 
     function renderizarConteudo() {
         return (
             <>
+                <Head>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                        if(!document.cookie.includes("admin-template-auth")) {
+                        window.location.href="/autenticacao"
+                        }
+                        `
+                        }}
+                    />
+                </Head>
                 {props.children}
             </>
         )
@@ -18,14 +30,14 @@ export default function ForcarAutenticacao(props){
     function renderizarCarregando() {
         return (
             <div className="flex justify-center items-center h-screen">
-                <Image src={loading} alt="LOADING..."/>
+                <Image src={loading} alt="LOADING..." />
             </div>
         )
     }
 
-    if(!carregando && usuario?.email) {
+    if (!carregando && usuario?.email) {
         return renderizarConteudo();
-    } else if(carregando) {
+    } else if (carregando) {
         return renderizarCarregando()
     } else {
         Router.push('/autenticacao');
